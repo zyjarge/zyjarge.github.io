a6230191bf5f09f9734d3e901e8b8fb53586b8d7
zyjarge <zyjarge@163.com>
2021-03-01 21:57:21 +0800

    rebuilding site 2021年 3月 1日 星期一 21时57分21秒 CST

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-# if using a theme, replace with `hugo -t <YOURTHEME>`
-#hugo
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
 
 # Go To Public folder
 # cd docs
